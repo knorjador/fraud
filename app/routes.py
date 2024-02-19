7aa85c4c7835f2def13873d0f140e4e2907c3392
knorjador <dev@raph.email>
2024-02-19 16:15:22 +0100

    web base

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,19 @@ from app import app
 @app.route('/')
 def index():
     return flask.render_template('index.html')
+
+@app.route('/login')
+def login():
+    return flask.render_template('login.html')
+
+@app.route('/register')
+def register():
+    return flask.render_template('register.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return flask.render_template('dashboard.html')
+
+@app.route('/predict')
+def predict():
+    return flask.render_template('predict.html')
